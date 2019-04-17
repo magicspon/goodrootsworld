bd717b0a45de5aaf537afe1c068ff4acb2193cf9
Dave Stockley <magicspon@gmail.com>
2019-04-18 00:00:32 +0100

    holding page.. un momento

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,10 @@ import Layout from '@/container/Layout'
 
 function PageHome() {
 	return (
-		<Layout>
-			<div className="flex items-center flex-col justify-center">
-				<h1 className="mb-2">Good Roots World</h1>
-				<p>coming soon...</p>
-			</div>
-		</Layout>
+		<div className="flex items-center flex-col justify-center h-screen">
+			<h1 className="mb-2">Good Roots World</h1>
+			<p>coming soon...</p>
+		</div>
 	)
 }
 
