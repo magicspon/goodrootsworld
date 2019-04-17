e8b6d2f81f2e2cc313602290e62b2fc403005aed
Dave Stockley <magicspon@gmail.com>
2019-04-18 00:00:55 +0100

    yarp

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import Layout from '@/container/Layout'
 
 function PageHome() {
 	return (
-		<div className="flex items-center flex-col justify-center h-screen">
-			<h1 className="mb-2">Good Roots World</h1>
-			<p>coming soon...</p>
-		</div>
+		<Layout>
+			<div className="flex items-center flex-col justify-center">
+				<h1 className="mb-2">Good Roots World</h1>
+				<p>coming soon...</p>
+			</div>
+		</Layout>
 	)
 }
 
